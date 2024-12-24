3c83246ea125f61bb9c8eaa5b5163ca19623cac4
ZioFrank25 <fravit25@gmail.com>
2024-12-24 18:53:21 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const links = document.querySelectorAll('a'); // Seleziona tutti i link
+    const links = document.querySelectorAll('a');
 
     links.forEach(link => {
         link.addEventListener('click', event => {
-            // Intercetta il click sui link
             const targetUrl = link.href;
-
-            // Evita comportamenti di default
             event.preventDefault();
-
-            // Applica l'effetto fade-out
             document.body.classList.add('fade-out');
-
-            // Aspetta la fine dell'animazione e poi cambia pagina
             setTimeout(() => {
                 window.location.href = targetUrl;
-            }, 500); // 500ms = durata dell'animazione
+            }, 500);
         });
     });
 
-    // Quando la pagina carica, applica fade-in
     document.body.classList.add('fade-in');
+
+    const snowflakesCount = 100;
+
+    for (let i = 0; i < snowflakesCount; i++) {
+        const snowflake = document.createElement("div");
+        snowflake.classList.add("snowflake");
+        snowflake.textContent = "❄️"; // Usa il simbolo del fiocco di neve
+        document.body.appendChild(snowflake);
+
+        snowflake.style.left = `${Math.random() * 100}vw`;
+        snowflake.style.animationDuration = `${Math.random() * 3 + 2}s`;
+        snowflake.style.animationDelay = `${Math.random() * 5}s`;
+    }
 });
